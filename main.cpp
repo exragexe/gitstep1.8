d7258264863ca48068054b3da435f4206baa582e
- <78488184+exragexe@users.noreply.github.com>
2023-02-09 21:50:11 +0200

    ex4hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
 
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+    char* znak = new char() ;
+    int* first=new int(0);
+    int* second = new int(0);
+
+    cout <<"Calculator";
+    cout <<"Enter two numbers: ";
+    cin >> *first>>*second;
+    cout <<"Enter znak(+/-/*/:):";
+    cin >> *znak;
+    if (*znak == '+'){
+        cout << "Result: "<<*first + *second;
+    }
+    else if (*znak == '-'){
+        cout << "Result: "<<*first - *second;
+    }
+    else if (*znak == '*'){
+        cout << "Result: "<<*first * *second;
+    }
+    else if (*znak == '/'){
+        cout << "Result: "<<*first / *second;
+    }
+
+
+
+    return 0;
+}
