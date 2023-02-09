16f60774696d8cff2f23465c40ec94b06ea23cb5
- <78488184+exragexe@users.noreply.github.com>
2023-02-09 21:48:33 +0200

    ex1hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+    int* first_num = new int(0);
+    int* second_num = new int(0);
+    cout << "Enter two numbers: ";
+    cin >> *first_num >> *second_num;
+    if (*first_num > *second_num) {
+        cout << *first_num << " More then "<< *second_num;
+
+    }
+    else {
+        cout << *second_num << " More then " << *first_num;
+    }
+    system("pause");
+    return 0;
+}
