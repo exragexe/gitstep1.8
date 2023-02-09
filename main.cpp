3794b2deba4a1398904be75a60301b85483d440b
- <78488184+exragexe@users.noreply.github.com>
2023-02-09 21:50:38 +0200

    ex5hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
 
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+    srand(time(0));
+    int* size = new int(5);
+    int* first=new int(*size);
+    int* sum = new int(0);
+
+    for (size_t i = 0; i <= *size; i++)
+    {
+        *(first+i) = rand()% 10;
+        *sum += *(first+i);
+
+
+
+    }
+    cout <<" suma:" << *sum;
+
+
+
+    return 0;
+}
