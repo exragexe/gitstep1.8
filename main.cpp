8ef1d802864e4a09ddf16255cfd637bff5b8b79b
- <78488184+exragexe@users.noreply.github.com>
2023-02-09 21:49:43 +0200

    ex3hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
 
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+
+    int* first_num = new int(5);
+    int* second_num = new int(8);
+    *first_num = *first_num+ *second_num;
+    *second_num = *first_num- *second_num;
+    *first_num = *first_num- *second_num;
+    cout <<"first number = "<<*first_num <<"\t second number "<<*second_num;
+
+
+
+    return 0;
+}
