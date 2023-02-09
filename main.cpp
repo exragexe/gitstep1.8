cf8b4cf16530764a875304e26ebeedc076733790
- <78488184+exragexe@users.noreply.github.com>
2023-02-09 21:49:11 +0200

    ex2hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
 
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+    int* first_num = new int(0);
+    cout << "Enter number: ";
+    cin >> *first_num ;
+    if(*first_num ==0){
+        cout << *first_num <<" Equal to zero";
+    }
+    else if(*first_num > 0){
+        cout << *first_num<<" is positive";
+    }
+    else{
+        cout << *first_num<<" is negative";
+    }
+    system("pause");
+    return 0;
+}
